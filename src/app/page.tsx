e9b4b30c005b000715c2ac85986c7fc9251e7476
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-04-06 21:14:54 +0700

    adapter fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
-"use client  ";
+"use client";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
-    // <form
-    //   action={async () => {
-    //     "use server";
-    //     try {
-    //       await signIn("credentials", {
-    //         username: "1",
-    //         password: "test",
-    //         redirect: true,
-    //         redirectTo: "/dashboard",
-    //       });
-    //     } catch (e) {
-    //       console.log("lari sia naon nya anjintgl", e);
-    //     }
-    //   }}
-    // >
     <Button
       onClick={() =>
-        signIn("credentials", {
-          username: "1",
-          password: "test",
-          redirect: true,
+        signIn("google", {
           callbackUrl: "/dashboard",
         })
       }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import Cognito from "next-auth/providers/cognito";
-import Credentials from "next-auth/providers/credentials";
+
+import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./dbConnect";
 
 export const {
   handlers: { POST, GET },
@@ -9,33 +12,7 @@ export const {
   auth,
 } = NextAuth({
   session: { strategy: "jwt" },
-  providers: [
-    Credentials({
-      //   id: "1",
-      //   name: "credential1",
-      //   credentials: {
-      //     username: {
-      //       type: "text",
-      //     },
-      //     password: {
-      //       type: "password",
-      //     },
-      //   },
-      async authorize(credentials) {
-        const user = { id: "1", username: "1", password: "test" };
-        if (
-          user.password === credentials.password &&
-          user.username === credentials.username
-        ) {
-          return user;
-        }
-        return null;
-      },
-    }),
-    Cognito({
-      clientId: process.env.COGNITO_CLIENT_ID,
-      issuer: process.env.COGNITO_ISSUER,
-      clientSecret: process.env.COGNITO_CLIENT_SECRET,
-    }),
-  ],
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma as any),
+  ...authConfig,
 });
