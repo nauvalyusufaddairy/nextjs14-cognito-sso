b4428f551415cb920f485f228d1889331e65d048
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-04-11 15:08:17 +0700

    adding new middleware

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,3 @@
-export const PRIVATE_ROUTES = ["/dashboard"];
+export const PRIVATE_ROUTES = ["/private"];
 export const PUBLIC_ROUTES = ["/"];
 export const AUTHJS_PREFIX = "/api/auth";
---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { auth } from "@/auth";
+import NextAuth from "next-auth";
+import { PRIVATE_ROUTES, PUBLIC_ROUTES, AUTHJS_PREFIX } from "@/lib/routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isPublicRoutes = PUBLIC_ROUTES.includes(nextUrl.pathname);
+  const isPrivateRoutes = PRIVATE_ROUTES.includes(nextUrl.pathname);
+  const isNextauthApiPrefix = nextUrl.pathname.startsWith(AUTHJS_PREFIX);
+  const isLoggedIn = !!req.auth;
+
+  if (isNextauthApiPrefix) {
+    return null;
+  }
+  if (isPrivateRoutes) {
+    if (!isLoggedIn) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+    return null;
+  }
+
+  if (isPublicRoutes) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL("/private", nextUrl));
+    }
+    return null;
+  }
+});
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
