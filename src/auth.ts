3543e968ea6453b09e6020c2efe9b4ff3ecdf56a
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-04-11 16:58:07 +0700

    adding new prisma scheme

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/dbConnect";
 export const {
   auth,
   handlers: { GET, POST },
@@ -7,5 +9,7 @@ export const {
   signOut,
 } = NextAuth({
   session: { strategy: "jwt" },
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma as any),
   ...authConfig,
 });
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,17 +1,32 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined;
+// };
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
